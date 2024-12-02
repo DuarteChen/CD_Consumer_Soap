8b17697ef33a3b3918f589509646b18878e03078
Akashpreet Singh <akashpreetsingh739@gmail.com>
2024-12-02 22:06:52 +0000

    melhorando loop

--- src/main/java/default_package/RemoverConsultaServer.java
+++ src/main/java/default_package/RemoverConsultaServer.java
@@ -17,6 +17,7 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
  *       &lt;sequence&gt;
  *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
  *       &lt;/sequence&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
@@ -27,13 +28,16 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "removerConsulta_Server", propOrder = {
-    "arg0"
+    "arg0",
+    "arg1"
 })
 public class RemoverConsultaServer {
 
     protected int arg0;
 
-    /**
+    protected int arg1;
+
+	/**
      * Gets the value of the arg0 property.
      * 
      */
@@ -47,6 +51,22 @@ public class RemoverConsultaServer {
      */
     public void setArg0(int value) {
         this.arg0 = value;
+    }
+
+	/**
+     * Gets the value of the arg1 property.
+     * 
+     */
+    public int getArg1() {
+        return arg1;
+    }
+
+	/**
+     * Sets the value of the arg1 property.
+     * 
+     */
+    public void setArg1(int value) {
+        this.arg1 = value;
     }
 
 }
